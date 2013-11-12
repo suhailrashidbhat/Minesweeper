b39f31814a09c4ddcf85f070f04f338b352f6cc9
Suhail Rashid Bhat <suhail.bhat@crichq.com>
2013-11-12 22:21:43 +0530

    Initial Commit

---
+++ Minesweeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Minesweeper
+//
+//  Created by Suhail Rashid Bhat on 12/11/13.
+//  Copyright (c) 2013 SuhailBhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MineSweeperAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MineSweeperAppDelegate class]));
+    }
+}
